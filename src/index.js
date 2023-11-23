e3344a5c206a366e6945c9add4bb739144265f38
TFKChile <jescobarv@utem.cl>
2023-11-23 14:58:40 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import express from "express";
 import {loginRouter} from "../routes/login.js";
 import passport from "passport";
 import "../Middleware/google.js";
+import sqlite from 'sqlite3';
 
 const app = express();
+const sqlite3 = sqlite.verbose();
+
+const db = new sqlite3.Database('../db/Si.db', sqlite3.OPEN_READWRITE, (err) => { //
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Conectado a la base de datos.');
+});
 
 app.use(express.json());
 app.use(passport.initialize());
 
 app.use(
-    "/auth",
-    passport.authenticate("auth-google", {
+    "/auth", //ruta para la autenticacion
+    passport.authenticate("auth-google", {  
       scope: [
         "https://www.googleapis.com/auth/userinfo.profile",
         "https://www.googleapis.com/auth/userinfo.email",
