b20e349c65876d52acf4864ca6761ede19735934
TFKChile <102984977+TFKChile@users.noreply.github.com>
2023-11-25 05:18:43 -0300

    Update google.js

--- Middleware/google.js
+++ Middleware/google.js
@@ -13,7 +13,7 @@ passport.use(
     {
       clientID: process.env.clientID,
       clientSecret: process.env.clientSecret,
-      callbackURL: "http://159.223.193.199:9804/auth/google",
+      callbackURL: "http://localhost:5000/auth/google",
     },
     function (accessToken, refreshToken, profile, done) {
       const response = emails.includes(profile.emails[0].value);
